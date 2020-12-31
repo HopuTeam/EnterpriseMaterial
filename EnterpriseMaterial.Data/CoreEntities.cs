2d640e41d381acf6c527679eb259ab3e9b1cbb14
Skilemon <35643936+Skilemon@users.noreply.github.com>
2020-12-31 11:01:10 +0800

    Default project

---
+++ EnterpriseMaterial.Data/CoreEntities.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnterpriseMaterial.Data
+{
+    public class CoreEntities : DbContext
+    {
+        public CoreEntities(DbContextOptions<CoreEntities> options) : base(options) { }
+    }
+}
